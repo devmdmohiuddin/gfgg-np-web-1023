fa38da6c53ba8973ce1f1d8945ac02d5e297a7f2
devmdmohiuddin <devmdmohiuddin0241@gmail.com>
2023-09-20 12:05:12 +0600

    Home and about fully finished

--- src/js/index.js
+++ src/js/index.js
@@ -73,23 +73,18 @@
   carousel.addEventListener("mouseup", dragStop);
 
 
-//  kit ui
-let defaultTransform = 0;
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
 
-function goNext() {
-    defaultTransform = defaultTransform - 400;
-    var slider = document.getElementById("slider");
-    if (Math.abs(defaultTransform) >= slider.scrollWidth / 1.7) defaultTransform = 0;
-    slider.style.transform = "translateX(" + defaultTransform + "px)";
-}
-next.addEventListener("click", goNext);
-function goPrev() {
-    var slider = document.getElementById("slider");
-    if (Math.abs(defaultTransform) === 0) defaultTransform = 0;
-    else defaultTransform = defaultTransform + 400;
-    slider.style.transform = "translateX(" + defaultTransform + "px)";
-}
-prev.addEventListener("click", goPrev);
+  
+  const slidesContainer = document.querySelector(".slides-container");
+  const slideWidth = slidesContainer.querySelector(".slide").clientWidth;
+  const prevButton = document.querySelector(".prev");
+  const nextButton = document.querySelector(".next");
+  
+  nextButton.addEventListener("click", () => {
+    slidesContainer.scrollLeft += slideWidth;
+  });
+  
+  prevButton.addEventListener("click", () => {
+    slidesContainer.scrollLeft -= slideWidth;
+  });
 
